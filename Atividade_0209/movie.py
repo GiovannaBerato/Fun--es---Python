348d6aa5672c0b7b91dd771ae726a657a6d3f297
Giovanna Berato <giovanna.berato@gmail.com>
2024-09-02 22:43:53 -0300

    update

--- Atividade_0209/movie.py
+++ Atividade_0209/movie.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-filmes = pd.read_csv('movies/tmdb_5000_credits.csv')
+filmes = pd.read_csv('Atividade_0209/tmdb_5000_credits.csv')
 
 # Visualizando as primeiras linhas
 print(filmes.head())
@@ -18,7 +18,8 @@ def encontrarator(filmes, nome_ator):
     filmes_encontrados = []
 
 # Itere sobre as linhas do DataFrame
-    for row in filmes.iterrows():elenco = row["cast"]
+    for _ ,row in filmes.iterrows(): 
+        elenco = row["cast"]
     
 # Pegue o conteúdo da coluna 'Elenco'
 
@@ -28,6 +29,9 @@ def encontrarator(filmes, nome_ator):
     # Adicione o título do filme à lista
 
     return filmes_encontrados
+
+nome_ator = "Will Smith"
+filmes = encontrarator(filmes, nome_ator)
 
 print(f"Filmes com o ator {nome_ator}:")
 for filme in filmes:
